2782eb09ef80b2ec2104dd0f94178d303bae0cc0
Kenneth Reforma <kreforma@myseneca.ca>
2023-05-20 00:16:55 -0400

    useEffects

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,12 @@
 const Navbar = () => {
    return (
       <nav className="navbar">
-         <h1>Blog by React</h1>
+         <h1>REACT Project: Famous RDR2 Dialogues</h1>
          <div className="links">
             <a href="/">Home</a>
             <a href="/create" style={
                {color: "white", backgroundColor: '#f1356d', borderRadius: '8px'}
-            }>New Blog</a>
+            }>New Dialogue</a>
          </div>
       </nav>
    );
