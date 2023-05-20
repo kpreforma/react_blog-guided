fb83719553845fb1e64a983b55d28c5e437fd3d2
Kenneth Reforma <kreforma@myseneca.ca>
2023-05-19 23:23:11 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Home from './Home';
+import Navbar from './Navbar' 
+
+function App() {
+  // const title = 'Welcome to the new blog'
+  // const likes = 50
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
